240e25e8a76bcc3acd964cea15fadde763db8ae0
rjames187 <rory.james2021@gmail.com>
2022-05-01 20:38:40 -0500

    begin creating jsx form skeleton and state variables

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [inputData, setInputData] = useState({"text" : "", "expType" : ""});
+  const [displayData, setDisplayData] = useState({"infix" : '', "prefix" : '', "postfix" : '', "eval" : ""});
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Polish Notation Calculator</h1>
+      <h2>What kind of expression do you want to convert?</h2>
+      <form>
+        <input type="radio" id="infix-rb" name="expression-type" value="infix" checked></input>
+        <label for="infix-rb">Infix</label>
+        <input type="radio" id="prefix-rb" name="expression-type" value="prefix"></input>
+        <label for="prefix-rb">Prefix</label>
+        <input type="radio" id="postfix-rb" name="expression-type" value="postfix"></input>
+        <label for="postfix-rb">Postfix</label>
+        <input type="text" placeholder='enter an expression' required/>
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
